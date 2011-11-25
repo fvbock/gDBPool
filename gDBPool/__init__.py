7391e28cb08bdd1cee28a538a8d84a379e6741c3
Florian von Bock <f@vonbock.info>
2011-11-25 16:59:49 +0900

    deprecated import removed

--- gDBPool/__init__.py
+++
@@ -1,6 +0,0 @@
-from gDBPool.lib.psyco_ge import make_psycopg_green
-
-from gDBPool.gDBPoolError import DBInteractionException, DBPoolConnectionException, PoolConnectionException, StreamEndException
-
-
-__all__ = [ 'DBInteractionPool', 'DBConnectionPool' , 'PoolConnection' ]
