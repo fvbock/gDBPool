b0b84e506bbbd13667f82aef16564b933cc4c6ef
Florian von Bock <f@vonbock.info>
2012-03-18 03:14:41 +0900

    fixed merge fup

---
+++ gdbpool/channel_listener.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2011-2012 Florian von Bock (f at vonbock dot info)
+#
+# gDBPool - db connection pooling for gevent
+#
+# PGChannelListener - subscribes to a (NOTIFY) channel on postgres
+# via LISTEN and streams the events to the subscribes result_queues
+
+__author__ = "Florian von Bock"
+__email__ = "f at vonbock dot info"
+__version__ = "0.1.3"
+
+
+import gevent
+from gevent import monkey; monkey.patch_all()
+from psyco_ge import make_psycopg_green; make_psycopg_green()
+
+import psycopg2
+
+from gevent.select import select
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
+class PGChannelListenerException( Exception ):
+    pass
+
+
+def pipe_colon_unmarshall( payload_string ):
+    """
+    Convert a pipe seperated string of <key:value> pairs to a dict
+
+    :param string payload_string:
+    :rtype: dict
+    :returns: unmarshalled NOTIFY data
+    """
+
+    payload_data = {}
+    try:
+        for kv_pair in payload_string.split( '|' ):
+            k, v = kv_pair.split( ':', 1 )
+            payload_data[ k ] = v
+    except Exception, e:
+        raise PGChannelListenerException( "Unmarshalling of the LISTEN payload failed: %s" % ( e.message, ) )
+    return payload_data
+
+
+class PGChannelListener( object ):
+    """
+    A Listener for Postgres LISTEN/NOTIFY channels using gevent.
+
+    For each channel there will be one :class:`PGChannelListener` instance that fans
+    notifications out to the subscribed :class:`Queue`
+    """
+
+    def __new__(  cls, q, pool, channel_name, *args, **kwargs ):
+        if not hasattr( cls, '_instances' ):
+            cls._instances = {}
+        spawn = False
+        if not cls._instances.has_key( channel_name ):
+            cls._instances[ channel_name ] = object.__new__( cls )
+            cls._instances[ channel_name ].subscribers = {}
+            cls._instances[ channel_name ].pool = pool
+            cls._instances[ channel_name ].conn = pool.get( iso_level = ISOLATION_LEVEL_AUTOCOMMIT )
+            cls._instances[ channel_name ].cur = None
+            cls._instances[ channel_name ].channel_name = channel_name
+            spawn = True
+
+        # hai hai... using id for this kind of stuff is sort of dangerous.
+        # will come up with something less pointing gun at foot(TM). later. (TM).
+        cls._instances[ channel_name ].subscribers[ id( q ) ] = q
+        if spawn:
+            gevent.spawn( cls._instances[ channel_name ].listen ).join()
+        return cls._instances[ channel_name ]
+
+    def __init__( self, q, pool, channel_name ):
+        """
+        Create a Listener for a channel_name and pass the notifications to q result `Queue`.
+
+        :param gevent.Queue q: Queue to pass asynchronous payloads to
+        :param gDBPool.DBConnectionPool pool: Connection pool to get a connection from and execute ``LISTEN <channel_name>;`` on if no other instance listens on that channel already. In the latter case the q is just being subscribed to the channel.
+        :param string channel_name: channel to listen on. (``LISTEN <channel_name>;``)
+
+        :rtype: PGChannelListener
+        :returns: The PGChannelListener instance handling the channel
+        """
+
+        pass
+
+    def __del__( self ):
+        print "** __del__", self.channel_name
+        del PGChannelListener._instances[ self.channel_name ]
+
+    def unregister_queue( self, q_id ):
+        """
+        Unregister a Queue from its channel.
+
+        If it was the last subscriber stop listening on the channel and put the connection used back onto the pool.
+
+        :param q_id: The id() of the Queue to be unsubscribed from the channel
+        """
+
+        if self.stop_event.is_set():
+            return
+        del self.subscribers[ q_id ]
+        if len( self.subscribers ) == 0:
+            self.stop_event.set()
+            self.cur.close()
+            self.pool.put( self.conn )
+
+    def listen( self, unmarshaller = pipe_colon_unmarshall ):
+        """
+        Subscribe to the channel and send notification payloads to the
+        results Queue.
+
+        :param function unmarshaller: Function to pass the `notify.payload` string into for unmarshalling into python object (ie. dict) data
+        """
+
+        self.stop_event = stop_event = gevent.event.Event()
+        self.cur = self.conn.cursor()
+        self.cur.execute( "LISTEN %s;" % ( self.channel_name, ) )
+        while 1:
+            if self.stop_event.is_set():
+                return
+            # TODO: test select with timeout and try/except
+            # maybe try to listen on all channels with just one connection with short timeouts on the select
+            # and then cycle through...
+            if select( [ self.conn ], [], [] ) == ( [], [], [] ):
+                print "LISTEN timeout."
+            else:
+                if self.stop_event.is_set():
+                    return
+                self.conn.poll()
+                while self.conn.notifies:
+                    if self.stop_event.is_set():
+                        return
+                    notify = self.conn.notifies.pop()
+                    payload_data = unmarshaller( notify.payload )
+                    for q_id in self.subscribers.iterkeys():
+                        self.subscribers[ q_id ].put( payload_data )
+
---
+++ gdbpool/interaction_pool.py
@@ -0,0 +1,275 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2011-2012 Florian von Bock (f at vonbock dot info)
+#
+# gDBPool - db connection pooling for gevent
+
+__author__ = "Florian von Bock"
+__email__ = "f at vonbock dot info"
+__version__ = "0.1.3"
+
+
+import gevent
+from gevent import monkey; monkey.patch_all()
+from psyco_ge import make_psycopg_green; make_psycopg_green()
+import sys
+assert 'gdbpool.psyco_ge' in sys.modules.keys()
+
+import psycopg2
+import traceback
+
+from gevent.queue import Queue, Empty as QueueEmptyException
+from gevent.event import AsyncResult
+from types import FunctionType, MethodType, StringType
+from psycopg2 import DatabaseError
+psycopg2.extensions.register_type( psycopg2.extensions.UNICODE )
+psycopg2.extensions.register_type( psycopg2.extensions.UNICODEARRAY )
+from inspect import getargspec
+
+from connection_pool import DBConnectionPool
+from channel_listener import PGChannelListener
+from gdbpool_error import DBInteractionException, DBPoolConnectionException, PoolConnectionException, StreamEndException
+
+
+class DBInteractionPool( object ):
+    """
+    The DBInteractionPool manages `DBConnectionPool` instances and can run
+    queries or functions (ie. several queries wrapped in a function) on one of
+    these pools.
+    """
+
+    def __new__( cls, dsn, *args, **kwargs ):
+        if not hasattr( cls, '_instance' ):
+            cls._instance = object.__new__( cls )
+        return cls._instance
+
+    def __init__( self, dsn, pool_size = 10, pool_name = 'default',
+                  do_log = False ):
+        """
+        :param string dsn: DSN for the default `class:DBConnectionPool`
+        :param int pool_size: Poolsize of the first/default `class:DBConnectionPool`
+        :param string pool_name: Keyname for the first/default `class:DBConnectionPool`
+        :param bool do_log: Log to the console or not
+        """
+
+        if do_log == True:
+            import logging
+            logging.basicConfig( level = logging.INFO, format = "%(asctime)s %(message)s" )
+            self.logger = logging.getLogger()
+        self.do_log = do_log
+        self.request_queue = Queue( maxsize = None )
+        self.db_module = 'psycopg2'
+        self.conn_pools = {}
+        self.default_write_pool = None
+        self.default_read_pool = None
+        self.default_pool = None
+        self.active_listeners = {}
+        self.add_pool( dsn = dsn, pool_name = 'default', pool_size = pool_size,
+                       default_write_pool = True, default_read_pool = True,
+                       db_module = self.db_module )
+
+    def __del__( self ):
+        if self.do_log:
+            self.logger.info( "__del__ DBInteractionPool" )
+        for p in self.conn_pools:
+            self.conn_pools[ p ].__del__()
+
+    def __call__( self, *args, **kwargs ):
+        """ syntactic sugar for `:ref:DBInteractionPool.run` """
+        return self.run( *args, **kwargs  )
+
+    def add_pool( self, dsn = None, pool_name = None, pool_size = 10,
+                  default_write_pool = False, default_read_pool = False,
+                  default_pool = False, db_module = 'psycopg2' ):
+        """
+        Add a named `:class:DBConnectionPool`
+
+        :param string dsn: dsn
+        :param string pool_name: a name for the pool to identify it inside the DBInteractionPool
+        :param int pool_size: Number of connections the pool should have.
+        :param bool default_write_pool: Should the added pool used as the default pool for write operations?
+        :param bool default_read_pool: Should the added pool used as the default pool for read operations?
+        :param bool default_pool: Should the added pool used as the default pool? (*must* be a write pool)
+        :param string db_module: name of the DB-API module to use
+
+        .. note::
+            db_module right now ONLY supports psycopg2 and the option most likely will be removed in the future
+        """
+
+        if not self.conn_pools.has_key( pool_name ):
+            self.conn_pools[ pool_name ] = DBConnectionPool( dsn, db_module = self.db_module,
+                                                             pool_size = pool_size, do_log = self.do_log )
+            if default_write_pool:
+                self.default_write_pool = pool_name
+                if self.default_pool or self.default_pool is None:
+                    self.default_pool = pool_name
+            if default_read_pool:
+                self.default_read_pool = pool_name
+        else:
+            raise DBInteractionException( "Already have a pool with the name: %s. ConnectionPool not added!" % ( pool_name, ) )
+
+    @property
+    def pool( self ):
+        return self.conn_pools[ self.default_pool ]
+
+    def run( self, interaction = None, interaction_args = None,
+             get_result = True, is_write = True, pool = None, conn = None,
+             cursor = None, partial_txn = False, dry_run = False, *args,
+             **kwargs ):
+        """
+        Run an interaction on one of the managed `:class:DBConnectionPool` pools.
+
+        :param function|method interaction: The interaction to run. Either a SQL string or a function that takes at least a parameter `conn`.
+        :param string interaction_args: None,
+        :param bool get_result: call and return cursor.fetchall() when True - otherwise just return True as result if no exception was raised.
+        :param bool is_write: If the interaction has no side-effects set to `False`. Without naming a pool the default_read pool would be used.
+        :param string pool: Keyname of the pool to get the a connection from
+        :param connection conn: Pass in a `Connection` instead of getting one from the pool. (ie. for locks in transactions that span several interactions. Use `partial_txn = True` to retrieve the Connection and then pass it into the next interaction run.)
+        :param cursor cursor: Pass in a `Cursor` instead of getting one from the `Connection` (ie. for locks in transactions that span several interactions.  Use `partial_txn = True` to retrieve the Cursor and then pass it into the next interaction run.)
+        :param bool partial_txn: Return connection and cursor after executing the interaction (ie. for locks in transactions that span several interactions)
+        :param bool dry_run: Run the query with `mogrify` instead of `execute` and output the query that would have run. (Only applies to query interactions)
+        :param list args: positional args for the interaction
+        :param dict kwargs: kwargs for the interaction
+
+        :rtype: gevent.AsyncResult
+        :returns: -- a :class:`gevent.AsyncResult` that will hold the result of the interaction once it finished. When `partial_txn = True` it will return a dict that will hold the result, the connection, and the cursor that ran the transaction. (use for locking with SELECT FOR UPDATE)
+        """
+
+        async_result = AsyncResult()
+        if is_write:
+            use_pool = self.default_write_pool if pool is None else pool
+        else:
+            use_pool = self.default_read_pool if pool is None else pool
+
+        if not conn:
+            conn = self.conn_pools[ use_pool ].get()
+
+        if isinstance( interaction, FunctionType ) or isinstance( interaction, MethodType ):
+            def wrapped_transaction_f( async_res, interaction, conn = None,
+                                       cursor = None, *args ):
+                try:
+                    if not conn:
+                        conn = self.conn_pools[ use_pool ].get()
+                    kwargs[ 'conn' ] = conn
+                    if cursor:
+                        kwargs[ 'cursor' ] = cursor
+                    elif 'cursor' in getargspec( interaction )[ 0 ]:
+                        kwargs[ 'cursor' ] = kwargs[ 'conn' ].cursor()
+                    res = interaction( *args, **kwargs )
+                    if not partial_txn:
+                        async_res.set( res )
+                        if cursor and not cursor.closed:
+                            cursor.close()
+                    else:
+                        async_res.set( { 'result': res,
+                                         'connection': conn,
+                                         'cursor': kwargs[ 'cursor' ] } )
+                except DatabaseError, e:
+                    if self.do_log:
+                        self.logger.info( "exception: %s", ( e, ) )
+                    async_result.set_exception( DBInteractionException( e ) )
+                except Exception, e:
+                    if self.do_log:
+                        self.logger.info( "exception: %s", ( e, ) )
+                    async_result.set_exception( DBInteractionException( e ) )
+                finally:
+                    if conn and not partial_txn:
+                        self.conn_pools[ use_pool ].put( conn )
+
+            gevent.spawn( wrapped_transaction_f, async_result, interaction,
+                          conn = conn, cursor = cursor, *args )
+            return async_result
+
+        elif isinstance( interaction, StringType ):
+            def transaction_f( async_res, sql, conn = None, cursor = None,
+                               *args ):
+                try:
+                    if not conn:
+                        conn = self.conn_pools[ use_pool ].get()
+                    if not cursor:
+                        cursor = conn.cursor()
+                    if not dry_run:
+                        if interaction_args is not None:
+                            cursor.execute( sql, interaction_args )
+                        else:
+                            cursor.execute( sql )
+                        if get_result:
+                            res = cursor.fetchall()
+                        else:
+                            res = True
+                        if is_write and not partial_txn:
+                            conn.commit()
+                    else:
+                        res = cursor.mogrify( sql, interaction_args )
+                    if not partial_txn:
+                        cursor.close()
+                        async_res.set( res )
+                    else:
+                        async_res.set( { 'result': res,
+                                         'connection': conn,
+                                         'cursor': cursor} )
+                except DatabaseError, e:
+                    if self.do_log:
+                        self.logger.info( "exception: %s", ( e, ) )
+                    async_result.set_exception( DBInteractionException( e ) )
+                except Exception, e:
+                    traceback.print_exc( file = sys.stdout )
+                    # if is_write and partial_txn: # ??
+                    conn.rollback()
+                    if self.do_log:
+                        self.logger.info( "exception: %s", ( e, ) )
+                    async_result.set_exception( DBInteractionException( e ) )
+                finally:
+                    if conn and not partial_txn:
+                        self.conn_pools[ use_pool ].put( conn )
+
+            gevent.spawn( transaction_f, async_result, interaction,
+                          conn = conn, cursor = cursor, *args )
+            return async_result
+        else:
+            raise DBInteractionException( "%s cannot be run. run() only accepts FunctionTypes, MethodType, and StringTypes" % interacetion )
+
+    def listen_on( self, result_queue = None, channel_name = None, pool = None,
+                   cancel_event = None, sleep_cycle = 0.1 ):
+        """
+        Listen for asynchronous events on a named Channel and pass them to the result_queue
+
+        :param gevent.Queue result_queue: The :class:`gevent.Queue` to pass event payloads to
+        :param string channel_name: Name of the channel to LISTEN on
+        :param string pool: Name of the pool to get the connection from
+        :param gevent.Event cancel_event: A :class:`gevent.Event` which will break the listening loop when set
+        """
+
+        if self.db_module != 'psycopg2':
+            raise DBInteractionException( "This feature requires PostgreSQL 9.x." )
+        use_pool = self.default_write_pool if pool is None else pool
+        try:
+            def start_listener():
+                self.active_listeners[ channel_name ] = PGChannelListener( result_queue, self.conn_pools[ use_pool ], channel_name )
+
+            # do we need a listen loop for all PGChannelListeners? maybe one is enough...
+            def listen( result_queue, cancel_event ):
+                while 1:
+                    if cancel_event.is_set():
+                        self.active_listeners[ channel_name ].unregister_queue( id( result_queue ) )
+                        if self.do_log:
+                            self.logger.info( "stopped listening on: %s", ( channel_name, ) )
+                        break
+                    gevent.sleep( sleep_cycle )
+
+            listener_jobs = [ gevent.spawn( start_listener ),
+                              gevent.spawn( listen, result_queue, cancel_event ) ]
+            gevent.joinall( listener_jobs )
+        except Exception, e:
+            print "# FRAKK", e
+            if self.do_log:
+                self.logger.info( e )
+
+
+
+# TODO: make this an option...?
+# DEC2FLOAT = psycopg2.extensions.new_type(
+#     psycopg2.extensions.DECIMAL.values,
+#     'DEC2FLOAT',
+#     lambda value, curs: float( value ) if value is not None else None )
+# psycopg2.extensions.register_type( DEC2FLOAT )
